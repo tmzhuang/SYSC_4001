b4bd092591086412399cce66b2fb29dd0774e3ad
Brandon To <brandon_to@hotmail.com>
2015-09-29 19:13:47 -0400

    Set up infrastructure for sensor.c

--- Assignment 1/sensor.c
+++ Assignment 1/sensor.c
@@ -15,13 +15,63 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define DEFAULT_MAX_READING 100
+#define DEFAULT_THRESHOLD 90
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    char *name;
+    int threshold = DEFAULT_THRESHOLD;
+    int max_reading = DEFAULT_MAX_READING;
+    int sensor_reading;
+    int running = 1;
+    struct timeval t1, t2;
+
+    if (argc < 2)
     {
-        fprintf(stderr, "Usage: sensor [NAME] [THRESHOLD]\n");
+        fprintf(stderr, "Usage: sensor NAME [THRESHOLD] [MAX_READING]\n");
         exit(EXIT_FAILURE);
+    }
+
+    name = argv[1];
+
+    if (argc > 2)
+    {
+        threshold = atoi(argv[2]);
+    }
+
+    if (argc > 3)
+    {
+        max_reading = atoi(argv[3]);
+    }
+
+    if (threshold > max_reading)
+    {
+        fprintf(stderr, "THRESHOLD(%d) must not exceed MAX_READING(%d)\n", threshold, max_reading);
+        exit(EXIT_FAILURE);
+    }
+
+    // Initilize random number generator
+    srand(time(NULL));
+
+    gettimeofday(&t1, NULL);
+    while (running)
+    {
+        gettimeofday(&t2, NULL);
+        if ((t2.tv_sec - t1.tv_sec) >= 2)
+        {
+            sensor_reading = rand()%(max_reading+1);
+            printf("%d\n", sensor_reading);
+            if (sensor_reading >= threshold)
+            {
+                printf("Sensor reading exceeded THRESHOLD(%d)!\n", threshold);
+                running = 0;
+            }
+            gettimeofday(&t1, NULL);
+        }
     }
 
     exit(EXIT_SUCCESS);
